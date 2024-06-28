a6d15e6760adb2a5c880eb6706560c38ee28073a
ABBA3124 <davideabbadessa73@gmail.com>
2024-06-28 10:40:10 +0200

    Initial commit

---
+++ src/main/java/davideabbadessa/U2_W2_D5_Development_Spring/U2W2D5DevelopmentSpringApplication.java
@@ -0,0 +1,13 @@
+package davideabbadessa.U2_W2_D5_Development_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W2D5DevelopmentSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W2D5DevelopmentSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/davideabbadessa/U2_W2_D5_Development_Spring/U2W2D5DevelopmentSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package davideabbadessa.U2_W2_D5_Development_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W2D5DevelopmentSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
